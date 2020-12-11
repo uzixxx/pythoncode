178cd56d2bd4ea7122165e6a9019bb4b95ae1f6e
uzixxx <595865975@qq.com>
2020-12-11 13:36:27 +0800

    dev

--- local/folderHelper.pyw
+++ local/folderHelper.pyw
@@ -1,30 +1,35 @@
-#coding:utf-8
-import xlrd,wx,subprocess
+import subprocess
+import wx
+import xlrd
+
+
 def FileContent(filename, sheetname):
     table = xlrd.open_workbook(filename)
     sheetcontent = table.sheet_by_name(sheetname)
     colesNum = sheetcontent.nrows
     titlecontent = sheetcontent.row_values(0)
-    result=[]
-    for i in range(1,colesNum):
+    result = []
+    for i in range(1, colesNum):
         result.append(dict(zip(titlecontent, sheetcontent.row_values(i))))
     return result
-tempFile, IP_File_HASH, BUTTON_LIST, EVENT_LSIT = FileContent( "IP_link.xlsx", "Ip"),{},[],[]
+
+
+tempFile, IP_File_HASH, BUTTON_LIST, EVENT_LSIT = FileContent("IP_link.xlsx", "Ip"), {}, [], []
 for i in tempFile:
-    IP_File_HASH.update({i['Name']:i['Link']})
-IP_NAME_LIST=sorted(IP_File_HASH.keys())
+    IP_File_HASH.update({i['Name']: i['Link']})
+IP_NAME_LIST = sorted(IP_File_HASH.keys())
 app = wx.App()
-frame = wx.Frame(None,title="FolderHelper",size=(250,400))
-scroll,vbox = wx.ScrolledWindow(frame, -1),wx.BoxSizer(wx.VERTICAL)
+frame = wx.Frame(None, title="FolderHelper", size=(250, 400))
+scroll, vbox = wx.ScrolledWindow(frame, -1), wx.BoxSizer(wx.VERTICAL)
 scroll.SetScrollbars(1, 1, 250, 400)
 for i in range(len(IP_NAME_LIST)):
     BUTTON_LIST.append(wx.Button(scroll, label=IP_NAME_LIST[i], pos=(0, 25 + 25 * i)))
 for i in range(len(IP_NAME_LIST)):
     vbox.Add(BUTTON_LIST[i], 0, wx.EXPAND | wx.ALIGN_CENTER)
 for i in range(len(IP_NAME_LIST)):
-    EVENT_LSIT.append(lambda event,comm=IP_File_HASH[IP_NAME_LIST[i]]:subprocess.run(r'''start "" '''+'''"'''+comm+'''"''', shell=True))
+    EVENT_LSIT.append(lambda event, comm=IP_File_HASH[IP_NAME_LIST[i]]:subprocess.run(r'''start "" '''+'''"'''+comm+'''"''', shell=True))
 for i in range(len(IP_NAME_LIST)):
-    BUTTON_LIST[i].Bind(wx.EVT_BUTTON,EVENT_LSIT[i],BUTTON_LIST[i])
+    BUTTON_LIST[i].Bind(wx.EVT_BUTTON, EVENT_LSIT[i], BUTTON_LIST[i])
 scroll.SetSizer(vbox)
 frame.Show()
 app.MainLoop()
--- local/local.py
+++ local/local.py
@@ -1,8 +1,7 @@
 import time
-from functools import reduce
 import pandas as pd
 import cx_Oracle
-import os
+
 
 def performance(unit):
     def perf_decorator(f):
@@ -16,6 +15,7 @@ def performance(unit):
         return wrapper
     return perf_decorator
 
+
 def log(prefix):
     def log_decorator(f):
         def wrapper(*args, **kw):
@@ -24,26 +24,28 @@ def log(prefix):
         return wrapper
     return log_decorator
 
-#实现查询并返回dataframe
+
+# 实现查询并返回dataframe
 @log('DEBUG')
 def query(sql):
     
-    host = "localhost"  #数据库ip
-    port = "1521"   #端口
-    sid = "orcl"  #数据库名称
+    host = "localhost"  # 数据库ip
+    port = "1521"   # 端口
+    sid = "orcl"  # 数据库名称
     dsn = cx_Oracle.makedsn(host, port, sid)
     
-    #scott是数据用户名，tiger是登录密码（默认用户名和密码）
+    # scott是数据用户名，tiger是登录密码（默认用户名和密码）
     conn = cx_Oracle.connect("bmi", "bmi", dsn)  
 
-    #使用pandas的read_sql函数，可以直接将数据存放在dataframe中
-    results = pd.read_sql(sql,conn)   
+    # 使用pandas的read_sql函数，可以直接将数据存放在dataframe中
+    results = pd.read_sql(sql, conn)
 
     conn.close()
     return results
 
+
 @log('DEBUG')
-def r_sql(name,st,et):
+def r_sql(name, st, et):
     
     # sql文件夹路径
     sql_path = ''
@@ -55,7 +57,7 @@ def r_sql(name,st,et):
     sql = open(sql_path + sql_file, 'r', encoding='utf-8')
     # 此时 sqltxt 为 list 类型
     sqltxt = sql.readlines()
-    #sqltxt = sql.read()
+    # sqltxt = sql.read()
     
     # 读取之后关闭文件
     sql.close()
@@ -63,18 +65,19 @@ def r_sql(name,st,et):
     # list 转 str
     sql = "".join(sqltxt)
 
-    sql_new = sql.replace('&begin_mon',"'"+st+"'").replace('&end_mon',"'"+et+"'")
+    sql_new = sql.replace('&begin_mon', "'"+st+"'").replace('&end_mon', "'"+et+"'")
     
     return sql_new
 
-#print(r_sql('local.sql','20180101','20190101'))
+# print(r_sql('local.sql','20180101','20190101'))
+
 
 cmd = input('请输入指令：')
 if cmd == '1':
     st = input('请输入开始时间：')
     et = input('请输入结束时间：')
-    sql = r_sql('local.sql',st,et)
-    #print(sql)
+    sql = r_sql('local.sql', st, et)
+    # print(sql)
     results = query(sql)
     results.to_csv('单据.csv', index=False, header=True,encoding="gbk")
 else:

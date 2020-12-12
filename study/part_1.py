3d674caf69c2249829871f68441eada43b6b3265
uzixxx <595865975@qq.com>
2020-12-12 21:00:25 +0800

    modify

--- study/part_1.py
+++ study/part_1.py
@@ -1,4 +1,11 @@
-motorcycles = ['honda','yamaha','suzuki']
-motorcycles.insert(0, 'ducati')
+motorcycles = ['honda', 'yamaha', 'suzuki']
+# motorcycles.append('ducati')
+# motorcycles.insert(0, 'ducati')
+# del motorcycles[0]
+# popped_motorcycles = motorcycles.pop()
+# print(popped_motorcycles)
+# first_owned = motorcycles.pop(0)
+# print(first_owned)
+motorcycles.remove('yamaha')
 print(motorcycles)
 

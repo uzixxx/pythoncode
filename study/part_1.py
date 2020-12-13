33eeb88f809997b42ead0c245a6f4815a3ff0243
uzixxx <595865975@qq.com>
2020-12-13 20:03:23 +0800

    modify

--- study/part_1.py
+++ study/part_1.py
@@ -6,6 +6,12 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 # print(popped_motorcycles)
 # first_owned = motorcycles.pop(0)
 # print(first_owned)
-motorcycles.remove('yamaha')
-print(motorcycles)
+# motorcycles.remove('yamaha')
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# cars.sort(reverse=True)
+# squares = [value**2 for value in range(1, 10)]
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+user_0 = {'username': 'zpf', 'age': 26, 'height': 175}
+print(user_0.items())
 

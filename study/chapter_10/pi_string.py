d875fdc1ef8ab01d4d177871eed0eb89cfde748d
uzixxx <595865975@qq.com>
2020-12-15 15:11:16 +0800

    modify

--- study/chapter_10/pi_string.py
+++ study/chapter_10/pi_string.py
@@ -10,8 +10,8 @@ for line in lines:
 # print(pi_string[:52] + "...")
 # print(len(pi_string))
 
-# birthday = input("Enter your birthday, in the form mmddyy: ")
-# if birthday in pi_string:
-#     print("Your birthday appears in the first million digits of pi!")
-# else:
-#     print("Your birthday does not appear in the first million digits of pi.")
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
